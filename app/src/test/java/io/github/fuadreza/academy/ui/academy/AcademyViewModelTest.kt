ad1ec1603b3d5471e758d428dea204358e28f86b
fuadreza <fuadreza1011@gmail.com>
2021-05-12 12:48:51 +0700

    Add Mockito unit testing for vviewmodel

--- app/src/test/java/io/github/fuadreza/academy/ui/academy/AcademyViewModelTest.kt
+++ app/src/test/java/io/github/fuadreza/academy/ui/academy/AcademyViewModelTest.kt
@@ -1,22 +1,36 @@
 package io.github.fuadreza.academy.ui.academy
 
-import org.junit.Test
-
-import org.junit.Assert.*
+import io.github.fuadreza.academy.data.CourseEntity
+import io.github.fuadreza.academy.data.source.AcademyRepository
+import io.github.fuadreza.academy.utils.DataDummy
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
 import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
+import org.mockito.junit.MockitoJUnitRunner
 
+@RunWith(MockitoJUnitRunner::class)
 class AcademyViewModelTest {
 
     private lateinit var viewModel: AcademyViewModel
 
+    @Mock
+    private lateinit var academyRepository: AcademyRepository
+
     @Before
     fun setUp() {
-        viewModel = AcademyViewModel()
+        viewModel = AcademyViewModel(academyRepository)
     }
 
     @Test
     fun getCourses() {
+        `when`(academyRepository.getAllCourses()).thenReturn(DataDummy.generateDummyCourses() as ArrayList<CourseEntity>)
         val courseEntities = viewModel.getCourses()
+        verify<AcademyRepository>(academyRepository).getAllCourses()
         assertNotNull(courseEntities)
         assertEquals(5, courseEntities.size)
     }
--- app/src/test/java/io/github/fuadreza/academy/ui/bookmark/BookmarkViewModelTest.kt
+++ app/src/test/java/io/github/fuadreza/academy/ui/bookmark/BookmarkViewModelTest.kt
@@ -1,22 +1,38 @@
 package io.github.fuadreza.academy.ui.bookmark
 
-import org.junit.Test
-
-import org.junit.Assert.*
+import io.github.fuadreza.academy.data.CourseEntity
+import io.github.fuadreza.academy.data.source.AcademyRepository
+import io.github.fuadreza.academy.utils.DataDummy
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
 import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
+import org.mockito.junit.MockitoJUnitRunner
 
+@RunWith(MockitoJUnitRunner::class)
 class BookmarkViewModelTest {
 
     private lateinit var viewModel: BookmarkViewModel
 
+    @Mock
+    private lateinit var academyRepository: AcademyRepository
+
     @Before
     fun setup() {
-        viewModel = BookmarkViewModel()
+        viewModel = BookmarkViewModel(academyRepository)
     }
 
     @Test
     fun getBookmarks() {
+        `when`<ArrayList<CourseEntity>>(academyRepository.getBookmarkedCourses()).thenReturn(
+            DataDummy.generateDummyCourses() as ArrayList<CourseEntity>
+        )
         val courseEntities = viewModel.getBookmarks()
+        verify(academyRepository).getBookmarkedCourses()
         assertNotNull(courseEntities)
         assertEquals(5, courseEntities.size)
     }
